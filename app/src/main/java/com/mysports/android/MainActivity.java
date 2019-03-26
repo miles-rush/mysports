224e192eb6d16a99e18f5100daae3762e7d1d2ca
Persistr <755842816@qq.com>
2019-03-26 17:11:53 +0800

    初步完成运动轨迹和轨迹矫正.

--- app/src/main/java/com/mysports/android/MainActivity.java
+++ app/src/main/java/com/mysports/android/MainActivity.java
@@ -1,13 +1,87 @@
 package com.mysports.android;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.mysports.android.map.LocationUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private MapView mapView;
+
+    private LocationUtil locationUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn);
+        //权限申请
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        } else {
+            //locationUtil.requestLocation();
+        }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RecordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "必须同意所有权限才能使用本程序", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    //locationUtil.requestLocation();
+                } else {
+                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
 }
---
+++ app/src/main/java/com/mysports/android/RecordListActivity.java
@@ -0,0 +1,57 @@
+package com.mysports.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.mysports.android.map.DbAdapter;
+import com.mysports.android.map.PathRecord;
+import com.mysports.android.map.RecordAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RecordListActivity extends AppCompatActivity implements OnItemClickListener {
+    private RecordAdapter mAdapter;
+    private ListView mAllRecordListView;
+    private DbAdapter mDataBaseHelper;
+    private List<PathRecord> mAllRecord = new ArrayList<PathRecord>();
+    public static final String RECORD_ID = "record_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recordlist);
+        mAllRecordListView = (ListView) findViewById(R.id.recordlist);
+        mDataBaseHelper = new DbAdapter(this);
+        mDataBaseHelper.open();
+        searchAllRecordFromDB();
+        mAdapter = new RecordAdapter(this, mAllRecord);
+        mAllRecordListView.setAdapter(mAdapter);
+        mAllRecordListView.setOnItemClickListener(this);
+    }
+
+    private void searchAllRecordFromDB() {
+        mAllRecord = mDataBaseHelper.queryRecordAll();
+    }
+
+    public void onBackClick(View view) {
+        this.finish();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        PathRecord recorditem = (PathRecord) parent.getAdapter().getItem(
+                position);
+        Intent intent = new Intent(RecordListActivity.this,
+                MainActivity.class);
+        intent.putExtra(RECORD_ID, recorditem.getId());
+        startActivity(intent);
+    }
+}
